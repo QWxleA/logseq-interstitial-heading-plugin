c3874f750b708e772577745ab5451b15bf1fd90d
Alex Qwxlea <qwxlea@gmail.com>
2022-03-16 10:08:18 +0100

    better docs and correct shortcutes

--- index.ts
+++ index.ts
@@ -1,57 +1,83 @@
 import '@logseq/libs';
 import SettingSchemaDesc from '@logseq/libs/dist/LSPlugin.user';
 
-var choices = ["Entire graph","Limit to tag"]
+var simple = true;
+
 const settingsTemplate:SettingSchemaDesc[] = [{
+    key: "defaultTitle",
+    type: 'boolean',
+    default: false,
+    title: "Insert Header by default?",
+    description: "If true, \"<mod> t\" will insert a header, otherwise only the time",
+   },{
     key: "level",
     type: 'number',
     default: 3,
     title: "Title level?",
     description: "Insert timestamped heading level, default to 3 (### HH:MM title)",
-  }
+    }
 ]
 logseq.useSettingsSchema(settingsTemplate)
 
-async function updateBlock(block) {
-    var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes();
-    var level = logseq.settings.level
-      let content = /^#{1,6}\s+/.test(block.content)
+async function updateBlock(block,simple) {
+    let content = /^#{1,6}\s+/.test(block.content)
         ? block.content.replace(/^#{1,6}\s+/, '')
         : block.content;
-        console.log("A",logseq.settings.level)
-        await logseq.Editor.updateBlock(
-          block.uuid,
-          '#'.repeat(logseq.settings.level) + ' ' + time + ' ' + content
-        );
+    var today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes()
+    let timestamp = simple ? ' ' + time + ' ' : '**' + time + '** '
+    let header = simple ? '#'.repeat(logseq.settings.level) : ''
+    await logseq.Editor.updateBlock(
+            block.uuid,
+            header + timestamp + content
+            );
 }
 
-async function setLevel() {
+async function insertInterstitional(simple) {
   const selected = await logseq.Editor.getSelectedBlocks();
   if (selected && selected.length > 1) {
     for (let block of selected) {
-        updateBlock(block)
+        updateBlock(block, simple)
          }
   } else {
     const block = await logseq.Editor.getCurrentBlock();
     if (block?.uuid) {
-        updateBlock(block)
+        updateBlock(block, simple)
     }
   }
 }
 
 async function main() {
+    //logseq.Editor.registerSlashCommand('Create SmartBlock (guided)', async () => {
+    //  await logseq.Editor.insertAtEditingCursor(`{{renderer :smartblock, template name, button title, sibling?}} `);
+      // templaterBlock = await logseq.Editor.getCurrentBlock();
+    //});
+
     logseq.App.registerCommandPalette(
       {
         key: `interstial-time-stamp`,
-        label: `Insert interstitial heading`,
+        label: `Insert interstitial line`,
         keybinding: {
           mode: 'global',
           binding: 'mod+t',
         },
       },
       async () => {
-        await setLevel();
+        await insertInterstitional(logseq.settings.defaultTitle ? true : false);
+      }
+    );
+
+    logseq.App.registerCommandPalette(
+      {
+        key: `interstial-time-stamp-h`,
+        label: `Insert interstitial heading`,
+        keybinding: {
+          mode: 'global',
+          binding: 'mod+shift+t',
+        },
+      },
+      async () => {
+        await insertInterstitional(logseq.settings.defaultTitle ? false : true);
       }
     );
 }
